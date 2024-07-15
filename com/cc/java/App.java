345250bbbde7922291e5efd7afa38d4fdc75b941
ahormozfar <a.hormozfar@yahoo.de>
2024-07-15 12:34:43 +0200

    attributes & primitives

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -10,19 +10,33 @@ public class App {
         // cat --> Referenzvariable
         // Cat --> Referenztyp(Klasse)
         Cat cat = new Cat();
-        output("Blick von aussen: " + cat);
-        output("Blick von innen: " + cat.getInstanceVariable());
+        // output("Blick von aussen: " + cat);
+        // output("Blick von innen: " + cat.getInstanceVariable());
 
+        //1 String Ausgabe
+        output(cat.name);
+        output(cat.furColor);
+
+        //2 Integer Ausgabe
+        output("age = " + cat.age);
+        output(Integer.toString(cat.age));
         System.out.println("----------------------------");
+        output(String.valueOf(cat.age));
 
-        Cat cat2 = new Cat();
-        output("Blick von aussen: " + cat2);
-        output("Blick von innen: " + cat2.getInstanceVariable());
+        
+
+        // Cat cat2 = new Cat();
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
 
     }
 
       // Statische Methode, die in der Klasse ausgeführt wird ...
     public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+   
+    public static void outputInt(int outputStr){
         System.out.println(outputStr);
     }
    
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,19 @@ package com.cc.java;
 
 public class Cat 
 {
+
+    //Eigenschaft (Properties) / Felder (attributes)
+    String name = "Arian";
+    String furColor = "Blue";
+    
+    //primitives
+    int age;
+    
+    double dezimal = 1.2;
+    float  ganzeZahl =  1;
+    boolean wahrheit = true;
+    
+    
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
